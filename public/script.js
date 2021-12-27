fea5432b84065e6c354931f722da5554b73c5f12
mkttpiras <mkttpiras@gmail.com>
2021-12-27 11:29:06 +0100

    Added UI -2

--- public/script.js
+++ public/script.js
@@ -1,3 +1,7 @@
 fetch('/temperature')
     .then(results => results.text())
-    .then(text => console.log(text))
+    .then(text => {
+        console.log(text);
+        const temperatureDisplay = document.querySelector('#temperature-display');
+        temperatureDisplay.innerHTML = text;
+    })
